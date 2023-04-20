fd84c5e30085920cd951faa98213f012c6c0c2a4
M1urray <rknjonjo@gmail.com>
2023-04-20 09:04:06 +0300

    Least Recently Used caching module

---
+++ 0x01-caching/3-lru_cache.py
@@ -0,0 +1,42 @@
+#!/usr/bin/python3
+''' self descriptive code '''
+
+from collections import OrderedDict
+
+from base_caching import BaseCaching
+
+
+class LRUCache(BaseCaching):
+    """Represents an object that allows storing and
+    retrieving items from a dictionary with a LRU
+    removal mechanism when the limit is reached.
+    """
+    def __init__(self):
+        """Initializes the cache.
+        """
+        super().__init__()
+        self.lru_order = OrderedDict()
+
+    def put(self, key, item):
+        """Adds an item in the cache.
+        """
+        if key and item:
+            self.lru_order[key] = item
+            self.lru_order.move_to_end(key)
+            self.cache_data[key] = item
+
+        if len(self.cache_data) > BaseCaching.MAX_ITEMS:
+            item_discarded = next(iter(self.lru_order))
+            del self.cache_data[item_discarded]
+            print("DISCARD:", item_discarded)
+
+        if len(self.lru_order) > BaseCaching.MAX_ITEMS:
+            self.lru_order.popitem(last=False)
+
+    def get(self, key):
+        """Retrieves an item by key.
+        """
+        if key in self.cache_data:
+            self.lru_order.move_to_end(key)
+            return self.cache_data[key]
+        return None
